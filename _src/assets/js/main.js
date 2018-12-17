9cb9188671753ced8ebca5904a0ef93a6ce72a65
Silvia García Cacho <silvia@garca.info>
2018-12-17 11:29:55 +0100

    Add list with favorites

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -12,7 +12,7 @@ let li = '';
 
 function searchIt(){
   listSelector.innerHTML = '';
-  console.log('pinchaste!')
+  console.log('pinchaste!');
   fetch(`http://api.tvmaze.com/search/shows?q=${inputSelector.value}`)
     .then(function(response){
       return response.json();
@@ -20,7 +20,7 @@ function searchIt(){
     .then(function(data){
 
       for(let i=0; i<data.length; i++){
-        const serie = data[i].show
+        const serie = data[i].show;
         const serieTitle = serie.name;
         //si la serie no tiene cartel, metemos una imagen de relleno de placehoder.com
         let serieImage = '';
@@ -32,39 +32,46 @@ function searchIt(){
 
         //Con los resultados, pintar un li con la tarjeta que muestre el cartel y el título de la serie
         li = document.createElement('li');
-        const liContent = `<div class="list-element"><img class ="list-element-image" src="${serieImage}" alt="cartel promocional de ${serieTitle}"><h2 class="list-element-title">${serieTitle}</h2></div>`;
+        const liContent = `<div class="list-element" id="${serieTitle}"><img class ="list-element-image" src="${serieImage}" alt="cartel promocional de ${serieTitle}"><h2 class="list-element-title">${serieTitle}</h2></div>`;
         li.innerHTML = liContent;
         listSelector.appendChild(li);
 
         //BUSCAR LA FORMA DE SACAR FUERA DESDE AQUI
         const divSerie = li.querySelector('.list-element');
-        function favoriteIt(e){
-          this.classList.toggle('favorite-element');
-          //INTENTANDO HACER EL LOCALSTORAGE
-          if (this.classList.contains('favorite-element')){
-            localStorage.setItem('favorite');
-          } else {
-            localStorage.removeItem('favorite');
-        }
+
+        //al hacer click sobre un resultado, cambia el color de fondo y se pone un borde en la tarjeta para marcarlo como 'favorito'
+        divSerie.addEventListener('click', favoriteIt);
+        //HASTA AQUI
+
       }
 
-        divSerie.addEventListener('click', favoriteIt);
-      //HASTA AQUI
-
-    }
-
-  })
-  .catch(error => console.log(`¡Upsi! Ha sucedido un error: ${error}`));
+    })
+    .catch(error => console.log(`¡Upsi! Ha sucedido un error: ${error}`));
 
 }
+
+const favorites = [];
+
+function favoriteIt(){
+  this.classList.toggle('favorite-element');
+  //INTENTANDO HACER EL LOCALSTORAGE
+  if (this.classList.contains('favorite-element')){
+    favorites.push(this.id);
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+  } else {
+    localStorage.removeItem('favorite');
+    favorites.splice(favorites.indexOf(this.id), 1);
+  }
+  console.log(favorites);
+}
+
 
 btnSelector.addEventListener('click', searchIt);
 
 
 
-
 //FAVORITO
-//al hacer click sobre un resultado, cambia el color de fondo y se pone un borde en la tarjeta para marcarlo como 'favorito'
+
 //almacenamos la info de los favoritos en LocalStorage para que aparezca marcada cuando volvamos a buscar
 
 
