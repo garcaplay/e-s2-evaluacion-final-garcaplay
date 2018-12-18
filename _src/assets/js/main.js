8c77cf7404f54c363a5bfc431e5b811c0bb9eebd
Silvia García Cacho <silvia@garca.info>
2018-12-18 08:13:16 +0100

    Solve bug on array length and separate from .then some code strings that are not necessary inside it

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,6 +5,13 @@ const btnSelector = document.querySelector('.main__searcher-btn');
 const listSelector = document.querySelector('.main__results-list');
 let li = '';
 
+let favorites;
+if (localStorage.getItem('favorites')) {
+  favorites = JSON.parse(localStorage.getItem('favorites'));
+} else {
+  favorites = [];
+}
+
 //BUSCADOR
 //Al hacer clic sobre el botón de 'Buscar'
 //recogemos el texto del campo de búsqueda y lo metemos a la url de la API
@@ -19,56 +26,38 @@ function searchIt(){
 
     })
     .then(function(data){
-      for(let i=0; i<data.length; i++){
-        const serie = data[i].show;
-        const serieTitle = serie.name;
-        const serieLink = serie.url;
-        let serieImage = '';
-        if (serie.image) {
-          serieImage = serie.image.medium;
-        } else {
-          serieImage = 'https://via.placeholder.com/210x295/cccccc/666666/?text=TV';
-        }
-
-        //Intento de crear mensaje si la búsqueda no da resultado
-        if(data.length === 0){
-          console.log('no hay nada');
-          const message = `<div class="error-message">
-            No hemos encontrado ningún resultado para ${inputSelector.value} :(
-            </div>`;
-          listSelector.appendChild(message);
-        }
-        //
-
-        li = document.createElement('li');
-        const liContent = `<div class="list-element" id="${serieTitle}">
-          <i class="favorite-btn fas fa-star-half"></i>
-          <a class="list-link" href="${serieLink}">
-          <img class ="list-element-image" src="${serieImage}" alt="cartel promocional de ${serieTitle}">
-          <h2 class="list-element-title">${serieTitle}</h2>
-          </a>
+      if(data.length === 0){
+        console.log('no hay nada');
+        const message = `<div class="error-message">
+          No hemos encontrado ningún resultado para tu búsqueda :(
           </div>`;
-        li.innerHTML = liContent;
-        listSelector.appendChild(li);
-
-        //BUSCAR LA FORMA DE SACAR FUERA DESDE AQUI
-        const divSerie = li.querySelector('.list-element');
-        const favIcon = li.querySelector('.favorite-btn');
-        let arrayFavorites = JSON.parse(localStorage.getItem('favorites'));
-
-        //intento de hacer lo de localstorage que no funciona
-
-        if (arrayFavorites.length>0) {
-          for (let j=0; j<arrayFavorites.length; j++){
-            if(arrayFavorites[j].includes(serieTitle)){
-              divSerie.classList.add('favorite-element');
-            }
+        listSelector.innerHTML = message;
+      } else {
+        for(let i=0; i<data.length; i++){
+          const serie = data[i].show;
+          const serieTitle = serie.name;
+          const serieLink = serie.url;
+          let serieImage = '';
+          if (serie.image) {
+            serieImage = serie.image.medium;
+          } else {
+            serieImage = 'https://via.placeholder.com/210x295/cccccc/666666/?text=TV';
           }
+
+          li = document.createElement('li');
+          const liContent = `<div class="list-element" id="${serieTitle}">
+            <i class="favorite-btn fas fa-star-half"></i>
+            <a class="list-link" href="${serieLink}">
+            <img class ="list-element-image" src="${serieImage}" alt="cartel promocional de ${serieTitle}">
+            <h2 class="list-element-title">${serieTitle}</h2>
+            </a>
+            </div>`;
+          li.innerHTML = liContent;
+          listSelector.appendChild(li);
+
+          favoriteClick(li, serieTitle);
+
         }
-
-        favIcon.addEventListener('click', favoriteIt);
-        //HASTA AQUI
-
       }
 
     })
@@ -76,12 +65,18 @@ function searchIt(){
 
 }
 
-//En https://www.taniarascia.com/how-to-use-local-storage-with-javascript/ recomiendan hacer este if statement si queremos que se guarde la info de localStorage se mantenga a pesar de cerrar o recargar la página, si pusiéramos let favorites = [] --> siempre empezaríamos con un array vacío en lugar de comprobar si esa info existe ya
-let favorites;
-if (localStorage.getItem('favorites')) {
-  favorites = JSON.parse(localStorage.getItem('favorites'));
-} else {
-  favorites = [];
+function favoriteClick(li, serieTitle){
+
+  const divSerie = li.querySelector('.list-element');
+  const favIcon = li.querySelector('.favorite-btn');
+
+  for (let j=0; j<favorites.length; j++){
+    if(favorites[j].includes(serieTitle)){
+      divSerie.classList.add('favorite-element');
+    }
+  }
+  favIcon.addEventListener('click', favoriteIt);
+
 }
 
 function favoriteIt(){
@@ -89,11 +84,11 @@ function favoriteIt(){
   favElement.classList.toggle('favorite-element');
   if (favElement.classList.contains('favorite-element') && !favorites.includes(favElement.id)){
     favorites.push(favElement.id);
-    localStorage.setItem('favorites', JSON.stringify(favorites));
   } else {
     favorites.splice(favorites.indexOf(favElement.id), 1);
-    localStorage.setItem('favorites', JSON.stringify(favorites));
   }
+  //metemos al key 'favorites' el valor favorites (tras stringifyearlo)
+  localStorage.setItem('favorites', JSON.stringify(favorites));
 }
 
 btnSelector.addEventListener('click', searchIt);
